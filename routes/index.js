c3eef070dacb17a0fa58e185edbf7819f5869751
ryan-love <mmccoopp99@gmail.com>
2020-07-29 15:34:22 +0100

    index.js commit using simple-git

--- routes/index.js
+++ routes/index.js
@@ -10,43 +10,18 @@ const simpleGitPromise = require('simple-git/promise')();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // change current directory to repo directory in local
-  shellJs.cd('C:\\Users\\Ryan Love\\WebstormProjects\\.dot');
-// Repo name
-  const repo = '.dot';  //Repo name
-// User name and password of your GitHub
-  const userName = process.env.GITU;
-  const password = process.env.GITP;
-// Set up GitHub url like this so no manual entry of user pass needed
-  const gitHubUrl = `https://github.com/ryan-love/.dot.git`;
-// add local git config like username and email
-  simpleGit.addConfig('user.email','mmccoopp99@gmail.com');
-  simpleGit.addConfig('user.name','ryan-love');
-// Add remore repo url as origin to repo
-  simpleGitPromise.addRemote('origin',gitHubUrl);
-// Add all files for commit
-  simpleGitPromise.add('.')
-      .then(
-          (addSuccess) => {
-            console.log(addSuccess);
-          }, (failedAdd) => {
-            console.log('adding files failed ' + failedAdd);
-          });
-// Commit files as Initial Commit
-  simpleGitPromise.commit('README from simple-git')
-      .then(
-          (successCommit) => {
-            console.log(successCommit);
-          }, (failed) => {
-            console.log('failed commmit '+ failed);
-          });
-// Finally push to online repository
-  simpleGitPromise.push('origin','test')
-      .then((success) => {
-        console.log('repo successfully pushed ' + JSON.stringify(success));
-      },(failed)=> {
-        console.log('repo push failed ' + failed);
-      });
+
+    simpleGit.status().then((data,err)=>{
+        console.log(data)
+        simpleGit.commit("index.js commit using simple-git","./routes/index.js").then((commit)=>{
+            console.log(commit)
+            simpleGit.push("https:github.com/ryan-love/.dot.git","master").then((push)=>{
+                console.log(push)
+            })
+        })
+
+    })
+
   res.render("index");
 });
 
