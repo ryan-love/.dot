c85ae5a3d4da4f70e3c7133d5135a1aa7790293e
ryan-love <mmccoopp99@gmail.com>
2020-07-29 15:42:14 +0100

    index.js commit using simple-git

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var Sequelize = require("sequelize");
+const sequelize = new Sequelize(`${process.env.DB_TYPE}://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_DEVELOPMENT_NAME}`);
+const Op = Sequelize.Op
+const Settings = sequelize.import("../db/models/settings")
+
 require("dotenv").config()
 // Simple-git without promise
-const simpleGit = require('simple-git')();
+const simpleGit = require('simple-git')({ maxConcurrentProcesses: 10 });
 // Shelljs package for running shell tasks optional
 const shellJs = require('shelljs');
 // Simple Git with Promise for handling success and failure
@@ -16,7 +21,13 @@ router.get('/', function(req, res, next) {
         simpleGit.commit("index.js commit using simple-git","./routes/index.js").then((commit)=>{
             console.log(commit)
             simpleGit.push("https://github.com/ryan-love/.dot.git","master").then((push)=>{
-                console.log(push)
+                Settings.create({
+                    setGit:{
+                        status:data,
+                        commit:commit,
+                        push:push
+                    }
+                })
             })
         })
 
